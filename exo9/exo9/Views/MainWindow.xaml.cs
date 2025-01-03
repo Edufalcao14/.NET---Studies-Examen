a645b26299e57d0719eb8e71084b4a7632efcbb2
edufalcao14 <eduardo.sampaio@student.vinci.be>
2025-01-03 03:24:14 +0100

    add : exo10

---
+++ exo9/exo9/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using exo9.ViewModels;
+using System.Windows;
+
+namespace exo9.Views;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.DataContext = new EmployeeVM();
+    }
+}
